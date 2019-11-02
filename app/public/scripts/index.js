a533fb30e28b13af664f1fdfe12ff0e8e3e988be
Sam Sverko <sam.sverko@gmail.com>
2019-11-02 13:07:54 -0400

    Build portfolio section on index page.
    - Add image assets.

--- app/public/scripts/index.js
+++ app/public/scripts/index.js
@@ -1,1 +1,9 @@
 console.info('index.js loaded.');
+
+function darkenPortfolioCoverImage(elementId, imageFile) {
+	document.getElementById(elementId).style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/static/images/index/${imageFile}')`;
+}
+
+function lightenPortfolioCoverImage(elementId, imageFile) {
+	document.getElementById(elementId).style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url('/static/images/index/${imageFile}')`;
+}
