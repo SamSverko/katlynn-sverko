0a0da4ebd9caff4b6192b557697287f4f068fcc3
Sam Sverko <sam.sverko@gmail.com>
2019-11-02 14:20:21 -0400

    Add proper page wrappers for keeping footer at bottom.
    - Update script to position scroll down element.

--- app/public/scripts/index.js
+++ app/public/scripts/index.js
@@ -1,5 +1,17 @@
 console.info('index.js loaded.');
 
+
+// MEET KATLYNN ========================================================
+window.addEventListener('resize', positionScrollDownElement);
+
+function positionScrollDownElement() {
+	document.querySelector('.meet-katlynn__scroll-down').style.top = window.getComputedStyle(document.querySelector('.meet-katlynn')).height;
+}
+
+positionScrollDownElement();
+
+
+// WORK ================================================================
 function darkenPortfolioCoverImage(elementId, imageFile) {
 	document.getElementById(elementId).style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('/static/images/index/${imageFile}')`;
 }
