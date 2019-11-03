6574525ddfc9c226c49c959db40048f00830207d
Sam Sverko <sam.sverko@gmail.com>
2019-11-03 07:33:26 -0500

    Fix animated hamburger menu

--- app/public/scripts/global.js
+++ app/public/scripts/global.js
@@ -15,15 +15,16 @@ function scrollToElement(elementId) {
 // PARTIAL | NAV BAR ===================================================
 function toggleNavBarLinks() {
 	const navBarElement = document.querySelector('.nav-bar-links');
+	const hamburgerElement = document.querySelector('.nav-bar__hamburger');
 	if (window.getComputedStyle(navBarElement).display == 'none') {
 		navBarElement.classList.remove('animation-fade-out');
 		navBarElement.classList.add('animation-fade-in');
 		navBarElement.style.display = 'flex';
-
+		hamburgerElement.classList.add('is-active');
 	} else {
 		navBarElement.classList.remove('animation-fade-in');
 		navBarElement.classList.add('animation-fade-out');
-
+		hamburgerElement.classList.remove('is-active');
 		setTimeout(() => {
 			navBarElement.style.display = 'none';
 		}, 500);
